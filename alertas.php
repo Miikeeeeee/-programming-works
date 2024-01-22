7da932678f228fe94750d87f754e1cc0b9e82a37
Mike <91901001+Miikeeeeee@users.noreply.github.com>
2024-01-22 21:38:32 +0000

    Delete alertas.php

--- alertas.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function alertas_error($mensagem)
-    {
-        $tipo = "alert-danger";
-        echo '<div class="alert ' . $tipo . ' alert-dismissible fade show" role="alert">' . $mensagem . '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
-    }
-?>
